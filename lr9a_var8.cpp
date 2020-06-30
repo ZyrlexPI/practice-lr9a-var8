0171d756b7628e7686e0f1bc7b12642db7f852e4
ZyrlexPI <neresto.al24@gmail.com>
2020-06-30 18:21:44 +0300

    Modified the CPP file.Added a function for searching for the maximum and minimum number of positive elements in columns, and column permutations.

--- lr9a_var8.cpp
+++ lr9a_var8.cpp
@@ -18,6 +18,42 @@ int** init_m(int** m, int n) //Initialization of the matrix
     return m;
 }
 
+void mod_m(int** m, int n)
+{
+    int i, j, max, min, inmax, inmin, temp, *k;
+    k = new int [n];
+    for (j = 0; j < n; j++) {//Search for the number of positive elements in columns
+        k[j] = 0;
+        for (i = 0; i < n; i++) {
+            if (m[i][j] > 0) {
+                k[j] = k[j] + 1;
+            }
+        }
+    }
+    max = k[0];
+    inmax = 0;
+    for (i = 1;i < n;i++) {//Search for the largest number of positive elements in a column
+        if (k[i] > max) {
+            max = k[i];
+            inmax = i;
+        }
+    }
+    min = k[0];
+    inmin = 0;
+    for (i = 1;i < n;i++) {//Search for the smallest number of positive elements in a column
+        if (k[i] < max) {
+            min = k[i];
+            inmin = i;
+        }
+    }
+    for (i = 0;i < n;i++) { //Permutation of columns
+        temp = m[i][inmax];
+        m[i][inmax] = m[i][inmin];
+        m[i][inmin] = temp;
+    }
+    delete[]k;
+}
+
 void out_m(int** m, int n) //Displaying the matrix on the screen
 {
     int i, j;
@@ -42,6 +78,9 @@ int main()
     } while (n <= 0);
     m = init_m(m, n);
     cout << "Initialized matrix: \n";
+    out_m(m, n);
+    mod_m(m, n);
+    cout << "The modified matrix: \n";
     out_m(m, n);
     delete[]m; //Freeing up memory used by the dynamic array
 }
